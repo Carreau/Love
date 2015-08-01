f7506cb6f4ec32047b9e499c4c8bf5b9227d0eb1
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-08-01 15:46:29 -0700

    None type as no is identifier

---
+++ build/flit/love/love.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+
+import emoji
+import os
+import sys
+import random
+import getpass
+import difflib
+import subprocess
+import webbrowser
+
+import pkgtools.pypi as pp
+import github
+from travispy import TravisPy
+
+from time import sleep
+from cookiecutter.main import generate_context, generate_files
+
+import keyring
+
+
+import logging
+
+
+
+GITHUB_NEW_TOKEN_URI = 'https://github.com/settings/tokens/new'
+
+log = logging.getLogger(__name__)
+log.setLevel(20)
+
+def main(proposal=None, target_dir=None):
+    """
+    completly set-up a package in the target dir (using cookie cutter) 
+        
+
+    """
+    if not target_dir:
+        target_dir = os.getcwd()
+        log.info('will use target dir', target_dir)
+
+    token = keyring.get_password('session','github_token')
+
+    if token is None:
+        GITHUB_NEW_TOKEN_URI = 'https://github.com/settings/tokens/new'
+        log.info(emoji.emojize(":heart_with_arrow: == LOVE == :heart_with_arrow:"))
+        log.info("I will need a new token to access your GitHub account, please give me a token that have `write:repo_hook` enable.")
+        log.info("I'll try to open github for you at the right page, otherwise please visit", GITHUB_NEW_TOKEN_URI)
+        sleep(5)
+        webbrowser.open_new_tab(GITHUB_NEW_TOKEN_URI)
+        token = getpass.getpass('github token:')
+        keyring.set_password('session','github_token', token)
+        log.info('token stored in your keyring as session:github_token')
+
+
+    # generate a python package name
+
+    adjectives = ['red','green','blue','purple','fluffy','soft','hard','golden','silver']
+    nouns = ['moon', 'frog', 'lake','orchid','sapphire','gem','sun','lily','ocean','lampshade','fish']
+    if not proposal.isidentifier() and len(proposal)>3:
+        log.info('package names should be valid python identifiers, and at least 3 letters long', proposal)
+        sys.exit(-1)
+
+
+    if not proposal:
+        proposal = random.choice(adjectives).capitalize()+random.choice(nouns).capitalize()
+    plist = None
+
+    #  compare name with existing package name, warn if too close
+
+    log.info('Comparing "%s" to other existing package names...' % proposal)
+    pypi = pp.PyPIXmlRpc()
+    # cache that on a weekly basis ?
+    if plist is None:
+        plist = pypi.list_packages()
+    closest = difflib.get_close_matches(proposal.lower(), map(str.lower, plist), cutoff=0.8)
+    if closest:
+        if proposal in closest:
+            log.error(proposal, 'already exists, maybe you would prefer to contribute to this package?')
+        else:
+            log.warn(proposal, 'name is close to the following package name:', closest)
+    else:
+
+        log.info(proposal, 'seems to have a sufficiently specific name, continuing...')
+
+
+    #  Actually authenticate with github 
+    #  Create (if do not exist) the named repo, and and clone URL.
+
+    gh = github.Github(token)
+    u = gh.get_user()
+    log.info('Logged in on GitHub as ', u.name)
+    from github import UnknownObjectException 
+    try:
+        repo = u.get_repo(proposal)
+        log.info('It appears like %s repository already exists, using it as remote' %proposal)
+    except UnknownObjectException:
+        repo = u.create_repo(proposal)
+
+    ssh_url = repo.ssh_url
+    slug = repo.full_name
+    log.info('Working with repository', slug)
+
+
+    # Clone github repo locally, over SSH an chdir into it
+
+    log.info("Cloning github repository locally")
+    subprocess.call(['git', 'clone' , ssh_url])
+    os.chdir(proposal)
+    log.info('I am now in ',os.getcwd())
+
+    repo, user = enable_travis(token, slug) 
+    project_layout(proposal, user, repo)
+
+
+# insert travis here. 
+
+def enable_travis(token, slug):
+    """
+    Enable Travis automatically for the given repo.
+
+    this need to have access to the GitHub token.
+    """
+
+    # Done with github directly. Login to travis
+
+    travis = TravisPy.github_auth(token, uri='https://api.travis-ci.org')
+    user = travis.user()
+    log.info('Travis user:', user.name)
+
+    # Ask travis to sync with github, try to fetch created repo with exponentially decaying time.
+
+    last_sync = user.synced_at
+    log.info('syncing Travis with Github, this can take a while...')
+    repo = travis._session.post(travis._session.uri+'/users/sync')
+    import time
+    for i in range(10):
+        try:
+            time.sleep((1.5)**i)
+            repo = travis.repo(slug)
+            if travis.user().synced_at == last_sync:
+                raise ValueError('synced not really done, travis.repo() can be a duplicate')
+            log.info('\nsyncing done')
+            break
+        except:
+            log.info('.')
+    ## todo , warn if not found
+
+
+    #  Enable travis hook for this repository
+
+    log.info('Enabling travis hook for this repository')
+    resp = travis._session.put(travis._session.uri+"/hooks/",
+                        json={
+                            "hook": {
+                                "id": repo.id ,
+                                "active": True
+                            }
+                        },
+                      )
+    if resp.json()['result'] is True:
+        log.info('Travis hook for this repository is now enabled.')
+        log.info('Continuous integration test should be triggered every time you push code to github')
+    else:
+        log.info("I was not able to set up Travis hooks... something went wrong.")
+
+    return repo, user
+
+def codecov():
+    pass
+
+def coveralls():
+    # ##  Do the same for read the doc.
+
+    # ## Shoudl we do https://coveralls.io/?
+
+    # ## Todo
+    #     - initiate template with something like cookie cutter
+    #     - handle case where use is not registered with one of the above services.
+    #     - easier way to get github token
+    pass
+
+def project_layout(proposal, user):
+
+    context_file = os.path.expanduser('~/.cookiecutters/cookiecutter-pypackage/cookiecutter.json')
+    context = generate_context(context_file)
+
+    os.chdir('..')
+    context['cookiecutter']['full_name'] = user.name
+    context['cookiecutter']['email'] = user.email
+    context['cookiecutter']['github_username'] = user.login
+    context['cookiecutter']['project_name'] = proposal
+    context['cookiecutter']['repo_name'] = proposal.lower()
+
+
+    generate_files(
+            repo_dir=os.path.expanduser('~/.cookiecutters/cookiecutter-pypackage/'),
+            context=context
+        )
+    os.chdir(proposal)
+
+    log.info(os.getcwd())
+    os.listdir('.')
+
+    subprocess.call(['git','add','.'])
+
+    subprocess.call(['git','commit',"-am'initial commit of %s'" % proposal])
+
+    subprocess.call(['git', "push", "origin", "master:master"])
+
+    webbrowser.open('https://travis-ci.org/{slug}'.format(slug=r.slug))
+
