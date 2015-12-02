3bb425afc147f475d4c37ac54b7e6ab843d2e22b
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-12-02 14:56:32 +0100

    bump to version 0.0.2

--- love/__init__.py
+++ love/__init__.py
@@ -4,7 +4,7 @@ Package creation and management, using Love.
 So now you can say that your packages was made with Love.
 """
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from .love import main
 
